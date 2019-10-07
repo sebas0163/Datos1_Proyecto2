1c43cf1eafdf9728fff4275280cdd342d7292899
erick <mazae2000@estudiantec.cr>
2019-10-07 15:42:30 -0600

    metodo para darle un valor a una palabra

---
+++ Text Finder/src/arboles/OperPalabras.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arboles;
+
+/**
+ *
+ * @author Erick
+ */
+public class operPalabras {
+    public int strToInt(String a){
+        a=a.toUpperCase(); 
+        String palabra=""; 
+        int len=a.length();
+        //len--;
+        for (int i=0;i<len;i++){
+            palabra+=Integer.toString(a.codePointAt(i));            
+        }
+        return Integer.parseInt(palabra);
+    }
+}
