208319aa6275ef7ce17d006d412a78365023d89a
erick <mazae2000@estudiantec.cr>
2019-10-14 21:04:39 -0600

    atributo lista lista txt

--- Text Finder/src/Logic/Documentos.java
+++ Text Finder/src/Logic/Documentos.java
@@ -1,16 +1,22 @@
 package Logic;
 
 import EstructurasDatos.ArbolBinarioBusqueda;
+import EstructurasDatos.DoubleEndedLinkedList;
+import Logic.ManejoArchivos;
+
 
 public class Documentos {
     private String ruta;
     private ArbolBinarioBusqueda<Integer> arbolPalabras;
     private String rutaTxt;
+    private DoubleEndedLinkedList listaTXT;
 
     public Documentos(String ruta1,String ruta2,ArbolBinarioBusqueda arbol){
         this.ruta = ruta1;
         this.rutaTxt = ruta2;
         this.arbolPalabras = arbol;
+        ManejoArchivos lec=new ManejoArchivos();
+        this.listaTXT= lec.read(rutaTxt);
     }
 
     public String getRuta() {
